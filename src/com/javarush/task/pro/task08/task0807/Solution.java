586cfa9bb142db27ce3ecf105fc56138db6b6bfb
Sp1inter <splinter2905@yandex.ru>
2025-03-10 14:19:20 +0300

    [9 уровень] [7 задача] Полезные нюансы. - Импорты: Part 2.

---
+++ src/com/javarush/task/pro/task08/task0807/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0807;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/* 
+Импорты: Part 2
+
+В классе Solution замени все полные имена классов в коде на короткие.
+
+Требования:
+•	В методе main должны быть только короткие имена классов: остальное менять нельзя.
+•	В классе Solution должно быть 3 импорта.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
