6e7cd21acedb42bef1e1f75490e82c69228a65c4
Sp1inter <splinter2905@yandex.ru>
2025-03-11 12:03:53 +0300

    [9 уровень] [14 задача] Полезные нюансы. - Флаги.

---
+++ src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task08.task0814;
+
+/* 
+Флаги
+
+Реализуй методы:
+
+setFlag(int number, int flagPos) — устанавливает значение "1" биту под индексом flagPos числа number и возвращает новое значение.
+resetFlag(int number, int flagPos) — устанавливает значение "0" биту под индексом flagPos числа number и возвращает новое значение.
+checkFlag(int number, int flagPos) — проверяет значение бита под индексом flagPos числа number и возвращает true, если значение "1" и false, если "0".
+Требования:
+•	Реализуй метод setFlag(int, int) согласно условию.
+•	Реализуй метод resetFlag(int, int) согласно условию.
+•	Реализуй метод checkFlag(int, int) согласно условию.
+*/
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+        return number | (1 << flagPos);
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        return number & ~(1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
