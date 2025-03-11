606a8ab8929e873907042fa5ff4c1877cec12937
Sp1inter <splinter2905@yandex.ru>
2025-03-11 12:15:31 +0300

    [9 уровень] [15 задача] Полезные нюансы. - Логические операторы.

---
+++ src/com/javarush/task/pro/task08/task0815/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0815;
+
+/* 
+Логические операторы
+
+Внеси исправления в метод taskForCondition(), чтобы он выводил в консоли число 30. Изменять можно только логические операторы: && на & и || на |.
+
+Требования:
+•	Нужно, чтобы метод taskForCondition() выводил число 30.
+•	В методе taskForCondition() не меняй ничего, кроме логических операторов.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        taskForCondition();
+    }
+
+    public static void taskForCondition() {
+        int number = 15;
+        boolean firstCondition = number < 15 & number > 25 & (number += 15) < 25;
+        boolean secondCondition = (number | 5) > 12 || (number = number + 2) > 40;
+        System.out.println(number);
+    }
+}
